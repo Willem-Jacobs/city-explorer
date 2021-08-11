b9b3600fc671941738f2c8cd957e00d0138941e7
Willem Jacobs <wamj58@gmail.com>
2021-08-11 12:13:23 -0500

    Display time lapsed for cache

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,10 +3,17 @@ import Card from "react-bootstrap/Card";
 
 class Weather extends React.Component {
   render() {
+    const timeLapse = (time) => {
+      const timeNow = Date.now();
+      return ((timeNow - time) / 1000).toFixed(2);
+    };
+
     const { item } = this.props;
     return (
       <>
-        <Card.Header>{item.date}</Card.Header>
+        <Card.Header>
+          {item.date} Data cached: {timeLapse(item.timestamp)} seconds ago.
+        </Card.Header>
         <Card.Body>
           <Card.Text>
             Forecast: <strong>{item.description}</strong>
